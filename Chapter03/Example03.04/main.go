8e9e75f600aeb5ddcf4ddb9ce0ce0632d155ac4a
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-03-01 14:36:37 +0800

    溢位（overflow）、越界繞回（wraparound）

--- Chapter03/Example03.04/main.go
+++ Chapter03/Example03.04/main.go
@@ -7,6 +7,7 @@
 */
 
 // 例、以下為溢位寫法
+/*
 package main
 
 import "fmt"
@@ -15,3 +16,21 @@ func main() {
 	var a int8 = 128
 	fmt.Println(a)
 }
+*/
+
+// 例、以下為觸發越界繞回
+package main
+
+import "fmt"
+
+func main() {
+	var a int8 = 125
+	var b uint8 = 253
+	for i := 0; i < 5; i++ {
+		a++
+		b++
+		fmt.Println(i, ")", "int8", a, "uint8", b)
+	}
+}
+
+//觸發越界繞回後，值會回到該型別的最低值。無號整數最低值為 0，有號整數最低值為負數。
