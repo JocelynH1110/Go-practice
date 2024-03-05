7af68d30ddda55d97392e9b5d0121591a81f0dd8
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-03-05 08:53:27 +0800

    用 for range 走訪

--- Chapter03/Example03.08/main.go
+++ Chapter03/Example03.08/main.go
@@ -1,5 +1,8 @@
 // 3-4-2 rune
-// 將字串切片轉換成 rune 切片（為了安全走訪每個字）
+
+// 將 byte 的字串切片轉換成 rune 切片（為了安全走訪每個字）
+
+// 當Go 語言編譯器發現你在嘗試走訪 rune 切片時，會自動把它轉成 range 迴圈，故也可以直接用 for range 走訪 rune 。
 
 package main
 
@@ -9,7 +12,14 @@ func main() {
 	username := "Sir_King_Über"
 	runes := []rune(username)
 
+	//用 rune 切片走訪
 	for i := 0; i < len(runes); i++ {
 		fmt.Print(string(runes[i]), " ")
+	}
+	fmt.Print("\n")
+
+	//用 for range 走訪
+	for _, v := range runes {
+		fmt.Print(string(v), " ")
 	}
 }
