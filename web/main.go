06d960e9ed38f52070489be1be4ab90241387585
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-06-12 00:40:27 +0800

    implement http handler

--- web/main.go
+++ web/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	r := http.NewServeMux()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "<h1>HOHOHO</h1>")
+	})
+	fmt.Println("Listening on :3000...")
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
