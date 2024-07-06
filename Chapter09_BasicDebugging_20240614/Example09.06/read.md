e753db5dd914dc8018fb7889e19d84c040e188f0
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-07-06 19:55:07 +0800

    Debugging - 撰寫單元測試

--- Chapter09_BasicDebugging_20240614/Example09.06/read.md
+++ Chapter09_BasicDebugging_20240614/Example09.06/read.md
@@ -4,8 +4,27 @@
 ### 撰寫測試檔
 Go 語言測試檔的名稱不重要，但結尾必須加上「_test」，例如 shape_test.go。
 在這檔案中，必須宣告一個測試用函式：
+
 ```go
 func Test<名稱>(t *testing.T)
 ```
  
-函式名稱也不重要，但必須以「Test」開頭，此函式會接收一個型別為 testing.T 的指標變數 t （來自 testing 套件）。
+* 函式名稱也不重要，但必須以「Test」開頭，例如 TestGetName。此函式會接收一個型別為 testing.T 的指標變數 t （來自 testing 套件）。
+* 跑測試：（加上旗標 v 好印出更詳盡的測試過程）
+```
+go test -v
+```
+    * 加上旗標 v 好印出更詳盡的測試過程。
+    * go test 會自動尋找目錄中的 Go 語言測試檔並執行。 
+    * 想執行特定的測試檔，可用 `go test 檔名` 的寫法。
+
+顯示結果：（正確）
+```go
+Example09.06/shape$ go test -v
+=== RUN   TestGetName
+--- PASS: TestGetName (0.00s)
+=== RUN   TestGetArea
+--- PASS: TestGetArea (0.00s)
+PASS
+ok  	github.com/jocelynh1110/go-practice/Chapter09_BasicDebugging_20240614/Example09.06/shape	0.002s
+```
--- Chapter09_BasicDebugging_20240614/Example09.06/shape/shape_test.go
+++ Chapter09_BasicDebugging_20240614/Example09.06/shape/shape_test.go
@@ -1,7 +1,9 @@
-package shape
+// 例子、撰寫兩個測試函式：
+package shape // 由於測試對象是 shape ，故宣告為同一套件
 
 import "testing"
 
+// 測試 shape 套件的 GetName() 函式
 func TestGetName(t *testing.T) {
 	// 測試用結構
 	triangle := Triangle{Base: 15.5, Height: 20.1}
@@ -11,4 +13,31 @@ func TestGetName(t *testing.T) {
 	if name := GetName(triangle); name != "三角形" {
 		t.Errorf("%T 形狀錯誤：%v", triangle, name) // 傳回值錯誤時回報測試錯誤
 	}
+	if name := GetName(rectangle); name != "長方形" {
+		t.Errorf("%T 形狀錯誤：%v", rectangle, name)
+	}
+	if name := GetName(square); name != "正方形" {
+		t.Errorf("%T 形狀錯誤：%v", square, name)
+	}
 }
+
+// 測試 shape 套件的 GetArea() 函式
+func TestGetArea(t *testing.T) {
+	// 測試用結構
+	triangle := Triangle{Base: 15.5, Height: 20.1}
+	rectangle := Rectangle{Length: 20, Width: 10}
+	square := Square{Side: 10}
+
+	if value := GetArea(triangle); value != 155.775 {
+		t.Errorf("%T 面積錯誤：%v", triangle, value) // 傳回值錯誤時回報測試錯誤
+	}
+	if value := GetArea(rectangle); value != 200 {
+		t.Errorf("%T 面積錯誤：%v", rectangle, value)
+	}
+	if value := GetArea(square); value != 100 {
+		t.Errorf("%T 面積錯誤：%v", square, value)
+	}
+}
+
+// 上面將測試用的結構寫在個別測試函式中，而非宣告在 shape 的套件層級變數，以免影響到 shape 套件本身。
+// 測試函式會使用這些結構來測試 shape 公開函式的傳回值，看看結果是否跟已知的正確結果相符。
